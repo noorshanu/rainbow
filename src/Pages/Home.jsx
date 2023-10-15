ced95b086566d7709696f8b307efd2d1bfea0f10
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-15 12:00:23 +0530

    new

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import About from '../components/About'
+import OurDelights from '../components/OurDelights'
 
 
 function Home() {
@@ -10,6 +11,9 @@ function Home() {
      <div className=' py-8'>
       <About/>
 
+     </div>
+     <div className='py-8'>
+      <OurDelights/>
      </div>
     </>
   )
---
+++ src/components/OurDelights.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function OurDelights() {
+  return (
+    <section>
+
+        <div className='container-wrapper bg-[#F42160]  pl-8 rounded-3xl'>
+            <div className=' relative py-8'>
+                <h1>Our Delights</h1>
+            </div>
+
+            <div className='flex justify-between'>
+                <div className='bg-white p-4'>
+                    <div className='bg-[#F42160]'>
+                        <h1>cone</h1>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default OurDelights

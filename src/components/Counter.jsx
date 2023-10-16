9cee4307021ca9c61384cfe099fa1753abe9513d
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-10-16 16:57:51 +0530

    new

---
+++ src/components/Counter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Counter() {
+  return (
+    <section>
+      <div className="container-wrapper">
+        <div className="bg-count ">
+          <div className="flex">
+            <div>
+              <p>Since</p>
+              <p>1987</p>
+            </div>
+            <div>
+              <p>Since</p>
+              <p>1987</p>
+            </div>
+            <div>
+              <p>Since</p>
+              <p>1987</p>
+            </div>
+            <div>
+              <p>Since</p>
+              <p>1987</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Counter;
